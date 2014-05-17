8987cff0e958f596f3390235ed7d631defba99d2
havargasp <alexandervargas1985@hotmail.com>
2014-05-17 11:59:09 -0500

    Creacion de la tabla auxiliar

--- PROYECTO FINAL - DDL.sql
+++ PROYECTO FINAL - DDL.sql
@@ -1,3 +1,13 @@
 CREATE TABLE AUXILIAR(
-
+AUX_ID VARCHAR2 (20) NOT NULL,
+NOMBRE_AUX VARCHAR2 (50) NOT NULL,
+APELLIDO_AUX VARCHAR2 (50) NOT NULL,
+TELEFONO_AUX VARCHAR2 (20) NOT NULL, -- Falta normalizar Direccion, Programa académico--
+CONSTRAINT PK_AUX PRIMARY KEY (AUX_ID)
 );
+
+COMMENT ON TABLE AUXILIAR IS 'Es la entidad que contiene los datos del auxiliar o estudiante que postula o puede observar las convocatorias.';
+COMMENT ON COLUMN AUXILIAR.AUX_ID IS 'Es la identificacion del auxiliar y la llave primaria';
+COMMENT ON COLUMN AUXILIAR.NOMBRE_AUX IS 'Es el nombre del auxiliar';
+COMMENT ON COLUMN AUXILIAR.APELLIDO_AUX IS 'Es el apellido del auxiliar';
+COMMENT ON COLUMN AUXILIAR.TELEFONO_AUX IS 'Es el telefono del auxiliar';
